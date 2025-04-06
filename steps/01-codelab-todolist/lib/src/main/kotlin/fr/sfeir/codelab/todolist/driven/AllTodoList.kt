471a22632f91cd96b52b3910f47c9a7568f0e7ac
Jsamson33 <julien.samson.33@gmail.com>
2025-04-06 12:44:56 +0200

    ✨ add todolist exercise

--- steps/01-codelab-todolist/lib/src/main/kotlin/fr/sfeir/codelab/todolist/driven/AllTodoList.kt
+++ steps/01-codelab-todolist/lib/src/main/kotlin/fr/sfeir/codelab/todolist/driven/AllTodoList.kt
@@ -1,10 +1,9 @@
 package fr.sfeir.codelab.todolist.driven
 
 import fr.sfeir.codelab.todolist.model.Todo
-import fr.sfeir.codelab.todolist.model.TodoId
 import fr.sfeir.codelab.todolist.model.UserId
 
 interface AllTodoList {
   infix fun UserId.add(todo: Todo)
-  fun getBy(userId: UserId, todoId: TodoId): Todo?
+  fun getAllOf(userId: UserId): List<Todo>?
 }
