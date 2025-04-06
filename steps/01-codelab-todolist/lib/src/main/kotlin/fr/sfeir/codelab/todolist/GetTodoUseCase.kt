b2edd94a84b83065b6755b9dde1bd96c8cf19a74
Jsamson33 <julien.samson.33@gmail.com>
2025-04-06 12:14:06 +0200

    ✅ add todolist solution

--- steps/01-codelab-todolist/lib/src/main/kotlin/fr/sfeir/codelab/todolist/GetTodoUseCase.kt
+++ steps/01-codelab-todolist/lib/src/main/kotlin/fr/sfeir/codelab/todolist/GetTodoUseCase.kt
@@ -4,13 +4,17 @@ import fr.sfeir.codelab.todolist.driven.AllTodoList
 import fr.sfeir.codelab.todolist.driving.GetTodo
 import fr.sfeir.codelab.todolist.driving.GetTodo.GetQuery
 import fr.sfeir.codelab.todolist.driving.GetTodo.GetTodoResult
+import fr.sfeir.codelab.todolist.driving.GetTodo.GetTodoResult.UserDoesNotExist
 
 class GetTodoUseCase(
   private val todoList: AllTodoList
 ) : GetTodo {
 
   override fun get(query: GetQuery): GetTodoResult {
-    TODO("Not yet implemented")
+    val result = todoList.getAllOf(query.userId) ?: return UserDoesNotExist
+    return result.firstOrNull { it.id == query.todoId }?.let {
+      GetTodoResult.Success(it)
+    } ?: GetTodoResult.TodoDoesNotExist
   }
 
 }
