9257f77387b8498321faf5a3ccd56d95fdf180a0
Jsamson33 <julien.samson.33@gmail.com>
2025-04-06 12:14:06 +0200

    ✅ add todolist solution

--- steps/01-codelab-todolist/lib/src/main/kotlin/fr/sfeir/codelab/todolist/AddTodoUseCase.kt
+++ steps/01-codelab-todolist/lib/src/main/kotlin/fr/sfeir/codelab/todolist/AddTodoUseCase.kt
@@ -2,13 +2,27 @@ package fr.sfeir.codelab.todolist
 
 import fr.sfeir.codelab.todolist.driven.AllTodoList
 import fr.sfeir.codelab.todolist.driving.AddTodo
-import fr.sfeir.codelab.todolist.driving.AddTodo.AddTodoResult
 import fr.sfeir.codelab.todolist.driving.AddTodo.AddCommand
+import fr.sfeir.codelab.todolist.driving.AddTodo.AddTodoResult
+import fr.sfeir.codelab.todolist.driving.AddTodo.AddTodoResult.AlreadyExists
+import fr.sfeir.codelab.todolist.driving.AddTodo.AddTodoResult.Success
 
 class AddTodoUseCase(
   private val todoList: AllTodoList
 ) : AddTodo {
   override fun add(command: AddCommand): AddTodoResult {
-    TODO("Not yet implemented")
+    val todos = todoList.getAllOf(command.userId)
+    return when {
+      (todos != null && todos.any { it.id == command.todo.id }) -> {
+        AlreadyExists
+      }
+
+      else -> {
+        with(todoList) {
+          command.userId add command.todo
+        }
+        Success
+      }
+    }
   }
 }
